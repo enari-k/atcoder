ef867755682b524e339f89c20a0e5c4da45552f3
狩野　琳太郎 <rintakano624@gmail.com>
2026-02-22 08:44:18 +0900

    ABC445とABC446の解答を追加

---
+++ ABC446/E/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using AtCoder;
+using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        var sc = new FastScanner();
+        var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+        Console.SetOut(sw);
+        int M = sc.Int();
+        int A = sc.Int();
+        int B = sc.Int();
+        long answer = 0;
+        for(int x = 0;x < M;x++)
+        {
+            for(int y = 0;y < M;y++)
+            {
+                bool[] amari = new bool[M];
+                List<long> kazu = new();
+                kazu.Add(x);
+                kazu.Add(y);
+                int pointer = 2;
+                while(true)
+                {
+                    kazu.Add((A*kazu[pointer-1]+B*kazu[pointer-2])%M);
+                    if(kazu[pointer]==y&&kazu[pointer]==x)
+                    {
+                        if(!amari[0]) answer++;
+                        break;
+                    }
+                    else
+                    {
+                        amari[kazu[pointer]]=true;
+                        pointer++;
+                    }
+                }
+            }
+        }
+        Console.WriteLine(answer);
+        Console.Out.Flush();
+    }
+
+    static int LowerBound(List<long> list, long value)
+    {
+        int left = 0, right = list.Count;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (list[mid] < value) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+
+    static int UpperBound(List<long> list, long value)
+    {
+        int left = 0, right = list.Count;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (list[mid] <= value) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+    static void D(object o)
+    {
+    #if DEBUG
+        Console.WriteLine(o);
+    #endif
+    }
+}
+
+class FastScanner
+{
+    private readonly Stream _stream;
+    private readonly byte[] _buffer = new byte[1024];
+    private int _ptr = 0;
+    private int _buflen = 0;
+
+    public FastScanner() { _stream = Console.OpenStandardInput(); }
+
+    private bool HasNextByte()
+    {
+        if (_ptr < _buflen) return true;
+        _ptr = 0;
+        _buflen = _stream.Read(_buffer, 0, _buffer.Length);
+        return _buflen > 0;
+    }
+
+    private byte ReadByte() => HasNextByte() ? _buffer[_ptr++] : (byte)0;
+
+    private static bool IsPrintableChar(int c) => 33 <= c && c <= 126;
+
+    private void SkipUnprintable()
+    {
+        while (HasNextByte() && !IsPrintableChar(_buffer[_ptr])) _ptr++;
+    }
+
+    public string Str()
+    {
+        SkipUnprintable();
+        var sb = new StringBuilder();
+        while (HasNextByte() && IsPrintableChar(_buffer[_ptr]))
+        {
+            sb.Append((char)ReadByte());
+        }
+        return sb.ToString();
+    }
+
+    public int Int()
+    {
+        long n = Long();
+        if (n < int.MinValue || n > int.MaxValue) throw new OverflowException();
+        return (int)n;
+    }
+
+    public long Long()
+    {
+        SkipUnprintable();
+        long n = 0;
+        bool minus = false;
+        byte b = ReadByte();
+        if (b == '-') { minus = true; b = ReadByte(); }
+        if (b < '0' || '9' < b) throw new FormatException();
+        while (true)
+        {
+            if ('0' <= b && b <= '9') { n *= 10; n += b - '0'; }
+            else if (b == (byte)0 || !IsPrintableChar(b)) return minus ? -n : n;
+            else throw new FormatException();
+            b = ReadByte();
+        }
+    }
+
+    public double Double() => double.Parse(Str());
+    public int[] IntArr(int n) { var a = new int[n]; for (int i = 0; i < n; i++) a[i] = Int(); return a; }
+    public long[] LongArr(int n) { var a = new long[n]; for (int i = 0; i < n; i++) a[i] = Long(); return a; }
+}
